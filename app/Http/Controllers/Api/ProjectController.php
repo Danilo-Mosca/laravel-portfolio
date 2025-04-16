8c3a9e62fbc12d736eecf528822c9b4d20f0b3a6
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-04-16 05:39:51 +0200

    Create the index and show routes and the related ProjectController with the functions that return the json contents

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    /* Visualizzo tutti i progetti */
+    public function index()
+    {
+        // prendo tutti i progetti da database:
+
+        // $projects = Project::all();     // mostro solo tutti i progetti, senza le tipologie e/o le tecnologie associati per quel progetto
+        $projects = Project::with('type')->get();    // mostro tutti i progetti e le relative tipologie associate
+        // $projects = Project::with('type', 'technologies')->get();   // mostro tutti i progetti con le tipologie e tecnologie associati
+
+        // dd($projects);
+
+        return response()->json(
+            [
+                "success" => true,
+                "data" => $projects
+            ]
+        );
+    }
+
+    /* Visualizzo il singolo progetto */
+    public function show(Project $project)
+    {
+        // dd($project);
+
+        // ATTENZIONE: se eseguissi il return direttamente così visualizzerei soltanto i campi dei progetto senza le tipologie e tecnologie.
+
+        // Voglio visualizzare le relazioni con le tabelle tipologie e tecnologie associate per quel progetto, devo richiamare il metodo load():
+        $project->load('type', 'technologies');  // così indico che voglio il mio progetto caricato con le tipologie e tecnologie ad esso associati (l'ordine di inserimento non è importante se non per l'ordine in cui verrà visualizzato nel json)   
+
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $project
+            ]
+        );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Mostra tutti i progetti:
+Route::get('projects', [ProjectController::class, 'index']);     //questa rotta risponderà all'indirizzo: http://127.0.0.1:8000/api/projects
+
+// Mostra il singolo progetto:
+Route::get('projects/{project}', [ProjectController::class, 'show']);   //questa rotta risponderà all'indirizzo: http://127.0.0.1:8000/api/project{id-del-project}
